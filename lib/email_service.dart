9cd6679fcc56096bdd45f12ecb5bc0642b7f34c9
Leng Sreyka <137022984+lengponay@users.noreply.github.com>
2024-12-26 14:11:22 +0700

    Update email_service.dart

--- lib/email_service.dart
+++ lib/email_service.dart
@@ -1,7 +1,7 @@
+// email_service.dart
 import 'dart:convert';
-import 'dart:typed_data';
-import 'package:flutter/foundation.dart'; // Untuk mendeteksi platform
-import 'package:http/http.dart' as http; // Untuk HTTP request
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
 
 class EmailService {
   static Future<bool> sendAudioEmail({
@@ -9,28 +9,13 @@ class EmailService {
     required String recipientEmail,
     required String userSubject,
     required String userMessage,
-    required String audioFilePath,
+    required String downloadUrl,
   }) async {
     try {
-      const String serviceId = 'service_2opkv0p'; 
-      const String templateId = 'template_zp8e9h5'; 
-      const String userId = '7TuPqzAsANRTuH5iN'; 
-
-      String base64Audio = '';
-      if ( audioFilePath.startsWith('blob:')) {
-        final response = await http.get(Uri.parse(audioFilePath));
-
-        if (response.statusCode == 200) {
-          final Uint8List audioBytes = response.bodyBytes;
-          base64Audio = base64Encode(audioBytes);
-        } else {
-          debugPrint('Failed to fetch blob: ${response.statusCode}');
-          return false;
-        }
-      } else {
-        debugPrint('Invalid audio file URL');
-        return false;
-      }
+    
+      const String serviceId = 'service_2opkv0p';
+      const String templateId = 'template_zp8e9h5';
+      const String userId = '7TuPqzAsANRTuH5iN';
 
       final Map<String, dynamic> emailData = {
         'service_id': serviceId,
@@ -41,7 +26,7 @@ class EmailService {
           'recipient_email': recipientEmail,
           'user_subject': userSubject,
           'user_message': userMessage,
-        'attachments': base64Audio,
+          'audio_url': downloadUrl,
         },
       };
 
